caba00929b158547d2efb0c233072c8d55cc6127
alan mulhall <alanmulhall@hotmail.com>
2014-06-23 22:23:29 -0700

    Added validation for email

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,3 @@
 class Artist < ActiveRecord::Base
+    validates :email, presence: true
 end
--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Artist, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it 'is not valid without a email' do
+        artist = FactoryGirl.build(:artist, email: '')
+        expect(artist).to_not be_valid
+    end
 end
